604bf71863ea38c64d99160609ac3dc78bb55b31
K Sandeep Kumar Reddy <ksandeepkumarreddy963@gmail.com>
2026-08-23 11:51:01 +0530

    Step 47: vectorize_counts_add_at

--- model.py
+++ model.py
@@ -453,8 +453,20 @@ def loop_fill_counts(n_matrix, data):
     return n_matrix
     pass
 
-# Step 47 - vectorize_counts_add_at (not yet solved)
-# TODO: implement
+# Step 47 - vectorize_counts_add_at
+import numpy as np
+
+def vectorize_counts_add_at(vocab_size, data):
+    """Build (V, V) bigram counts from a 1D id array using vectorized scatter-add."""
+    # TODO: allocate counts, then scatter-add 1 at each (data[:-1], data[1:]) pair
+    n_matrix = allocate_count_matrix(vocab_size)
+
+    cur = data[:-1]
+    nxt = data[1:]
+
+    np.add.at(n_matrix, (cur, nxt), 1)
+    return n_matrix
+    pass
 
 # Step 48 - add_one_smoothing (not yet solved)
 # TODO: implement
