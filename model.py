0576c6217f776d3d6be5c9c31a1ea9f7fd793319
K Sandeep Kumar Reddy <ksandeepkumarreddy963@gmail.com>
2026-08-25 14:46:47 +0530

    Step 59: one_hot_encode_batch

--- model.py
+++ model.py
@@ -570,8 +570,18 @@ def scale_w_small(w_matrix, scale):
     return arr
     pass
 
-# Step 59 - one_hot_encode_batch (not yet solved)
-# TODO: implement
+# Step 59 - one_hot_encode_batch
+import numpy as np
+
+def one_hot_encode_batch(ids, vocab_size):
+    """Convert a 1D array of token ids into a (N, vocab_size) one-hot matrix."""
+    # TODO: allocate an (N, vocab_size) zero matrix and set one 1 per row at ids[i]
+    N = len(ids)
+    arr = make_2d_zeros(N, vocab_size)
+    arr[np.arange(N), ids] = 1.0
+
+    return arr
+    pass
 
 # Step 60 - forward_logits_onehot (not yet solved)
 # TODO: implement
